277a3dba44a09cadc4a0e0d0f041df30ffdbf45a
Angel Solant <49602721+AngelFierz@users.noreply.github.com>
2024-09-11 20:26:33 -0700

    Change Scene, using the Build Index

---
+++ CambiarEscena.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour
+{
+	public int indiceDeEscena = 0; // Indice de la escena a la que quieres cambiar
+
+	public void CambiarEscenaOnClick()
+	{
+		SceneManager.LoadScene(indiceDeEscena);
+	}
+}
