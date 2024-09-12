5a3699c520f7aad86daa5d9b2e459689e676de23
Angel Solant <49602721+AngelFierz@users.noreply.github.com>
2024-09-11 20:37:19 -0700

    Update ActivarObjeto.cs

--- ActivarObjeto.cs
+++ ActivarObjeto.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class ActivarObjeto : MonoBehaviour
 {
-	// Objeto que se activará
+
 	public GameObject objetoAActivar;
 
-	// Tiempo en segundos para activar el objeto
+
 	public float tiempoDeEspera = 5f;
 
 	void Start()
 	{
-		// Inicia el temporizador para activar el objeto después de cierto tiempo
+		
 		Invoke("Activar", tiempoDeEspera);
 	}
 
 	void Activar()
 	{
-		// Activa el objeto especificado
+		
 		objetoAActivar.SetActive(true);
 	}
 }
+// I DONT WANNA BE, I DONT WANNA BE ME, I DONT WANNA BE, ME ANYMORE, 1 2 3 4
