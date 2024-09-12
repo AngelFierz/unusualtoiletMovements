68d7055eece076f6664cfd8e3ecb3ffab8cf75c6
Angel Solant <49602721+AngelFierz@users.noreply.github.com>
2024-09-11 20:36:43 -0700

    test

---
+++ ActivarObjeto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarObjeto : MonoBehaviour
+{
+	// Objeto que se activará
+	public GameObject objetoAActivar;
+
+	// Tiempo en segundos para activar el objeto
+	public float tiempoDeEspera = 5f;
+
+	void Start()
+	{
+		// Inicia el temporizador para activar el objeto después de cierto tiempo
+		Invoke("Activar", tiempoDeEspera);
+	}
+
+	void Activar()
+	{
+		// Activa el objeto especificado
+		objetoAActivar.SetActive(true);
+	}
+}
