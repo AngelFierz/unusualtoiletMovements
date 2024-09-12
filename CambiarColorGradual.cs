16426b46a888c6470177057e93cf0bd6b701f2c8
Angel Solant <49602721+AngelFierz@users.noreply.github.com>
2024-09-11 20:22:29 -0700

    Update CambiarColorGradual.cs

--- CambiarColorGradual.cs
+++ CambiarColorGradual.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 
 public class CambiarColorGradual : MonoBehaviour
 {
-	public float duracionTransicion = 5.0f; // Duración de la transición en segundos
-	public float velocidadCambioColor = 1.0f; // Ajusta la velocidad del cambio de color
+	public float duracionTransicion = 5.0f; 
+	public float velocidadCambioColor = 1.0f;
 
 	private Material material;
 	private float tiempoPasado = 0.0f;
 
 	void Start()
 	{
-		// Asegúrate de que el objeto tiene un material
+		// WHAT ARE YOU SAYING
 		Renderer renderer = GetComponent<Renderer>();
 		if (renderer != null)
 		{
@@ -22,29 +22,28 @@ public class CambiarColorGradual : MonoBehaviour
 		}
 		else
 		{
-			Debug.LogError("I LOVE YOU MARLEN");
-			enabled = false; // Desactiva el script si no hay Renderer y material
+			Debug.LogError("I LOVE YOU LESLY");
+			enabled = false; // a la mierda todo
 		}
 	}
 
 	void Update()
 	{
-		// Incrementa el tiempo transcurrido multiplicado por la velocidad
+		
 		tiempoPasado += Time.deltaTime * velocidadCambioColor;
 
-		// Calcula el progreso de la transición entre los colores
+		
 		float progreso = Mathf.Clamp01(tiempoPasado / duracionTransicion);
 
-		// Calcula el valor del componente Hue en función del tiempo pasado
+		
 		float hue = Mathf.Repeat(tiempoPasado, 1.0f);
 
-		// Convierte el valor de Hue a un color RGB
+		// Convierte el valor de Hue a un color rgb que se encuentra en la paleta esa, De todos modos solo yo me entiendo muejeje
 		Color colorActual = Color.HSVToRGB(hue, 1.0f, 1.0f);
 
-		// Asigna el color al material
 		material.color = colorActual;
 
-		// Si alcanza el final del espectro de colores, reinicia el tiempo
+		
 		if (progreso == 1.0f)
 		{
 			tiempoPasado = 0.0f;
